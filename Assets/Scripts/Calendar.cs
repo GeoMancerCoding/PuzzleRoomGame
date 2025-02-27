5e84751b30e97aeda8ea4fa48aca063f660fbba9
GeoMancerCoding <Gabrielerik.m@gmail.com>
2025-02-27 14:40:15 -0500

    Build conflicts
    Idk what's in here but git is yelling at me

---
+++ Assets/Scripts/Calendar.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Calendar : MonoBehaviour
+{
+    private Ray ray;
+    private RaycastHit hit;
+    public LayerMask CalendarTargetLayerMask;
+    public float TimeUntilColorStartsToChangeSecs = 3f;
+    public float ColorChangeDurationSecs = 4f;
+    private float elapsedTime = 0f;
+    private float elapsedColorChangeTime = 0f;
+    public Renderer March17thRenderer;
+    public Transform PaintingT;
+    public Interactable SafeInteractable;
+
+    void Update()
+    {
+        ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out hit, Mathf.Infinity, CalendarTargetLayerMask))
+        {
+            elapsedTime += Time.fixedDeltaTime;
+            if (elapsedTime > TimeUntilColorStartsToChangeSecs)
+            {
+                elapsedColorChangeTime += Time.fixedDeltaTime;
+                March17thRenderer.material.color = new Color(
+                    March17thRenderer.material.color.r,
+                    March17thRenderer.material.color.g,
+                    March17thRenderer.material.color.b,
+                    elapsedColorChangeTime / ColorChangeDurationSecs);
+                if (elapsedColorChangeTime > ColorChangeDurationSecs)
+                {
+                    GetComponent<Interactable>().LookAtPointOfInterest(PaintingT, true);
+                    SafeInteractable.CanBeInteractedWith = true;
+                    SafeInteractable.GetComponent<Animation>().Play("MovePainting");
+                }
+            }
+        }
+        else
+        {
+            elapsedTime = 0f;
+            elapsedColorChangeTime = 0f;
+        }
+    }
+}
